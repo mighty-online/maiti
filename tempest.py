c1ff13922cf7970a174ad3632c3c81606adb3562
SyphonArch <jake.hyun@hotmail.com>
2019-06-15 23:54:55 +0900

    Added docstring and authorship information

---
+++ tempest.py
@@ -0,0 +1,16 @@
+"""The AI worker module of Tranquil Tempest, the Mighty AI
+
+This module contains the ISMCTS-based AI algorithm for the game of Mighty, and along with it, the necessary game logic.
+This module has been written in mind of being called from tranquil.py, the front-end of Tranquil Tempest.
+
+Development started 2019/06/15
+"""
+
+__author__ = "Jake Hyun (SyphonArch)"
+__copyright__ = "Copyright 2019, The Mighty-Online Team"
+__credits__ = ["Jake Hyun (SyphonArch)"]
+__license__ = "MIT"
+__version__ = "0.1.0"
+__maintainer__ = "Jake Hyun (SyphonArch)"
+__email__ = "jake.hyun@hotmail.com"
+__status__ = "Development"
---
+++ tranquil.py
@@ -0,0 +1,16 @@
+"""The interface module of Tranquil Tempest, the Mighty AI
+
+This module handles incoming AI move requests from the backend server of Mighty-Online,
+and distributes actual work for the AI agents by running the AI worker module, tempest.py, on multiple threads.
+
+Development started
+"""
+
+__author__ = "Jake Hyun (SyphonArch)"
+__copyright__ = "Copyright 2019, The Mighty-Online Team"
+__credits__ = ["Jake Hyun (SyphonArch)"]
+__license__ = "MIT"
+__version__ = "0.1.0"
+__maintainer__ = "Jake Hyun (SyphonArch)"
+__email__ = "jake.hyun@hotmail.com"
+__status__ = "Development"
